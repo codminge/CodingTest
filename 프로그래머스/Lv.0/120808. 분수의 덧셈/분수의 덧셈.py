24b6c1b52b76d347273fbdc1cf49b27a3e9ef02f
codminge <126869709+codminge@users.noreply.github.com>
2024-01-15 05:06:08 +0900

    [level Lv.0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/Lv.0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -9,10 +9,12 @@ def solution(numer1, denom1, numer2, denom2):
     else:
         denom = denom1 * denom2
         numer = (numer1 * denom2) + (numer2 * denom1)
+        
     # 파이썬 최대공약수 내장함수
     gcd = math.gcd(denom, numer)
     if gcd != 1:
         numer, denom = divisionGCD(numer, denom, gcd)
+    
     answer.append(numer)
     answer.append(denom)
     return answer
