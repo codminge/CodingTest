3d065a802b3f6b4a0ea95aabbecd25a9311394c2
codminge <126869709+codminge@users.noreply.github.com>
2024-01-25 02:02:56 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -2,8 +2,7 @@ import re
 
 def solution(my_string):
     answer = 0
-    num = re.sub(r'[^0-9]', ' ', my_string)
-    numList = num.split()
+    numList = re.findall(r'\d+', my_string)
     for i in numList:
         answer = answer + int(i)
     return answer
