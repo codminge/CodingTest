cbadeb7f41a9fe5deecb27e1b6db5a7da365c302
codminge <126869709+codminge@users.noreply.github.com>
2024-01-28 22:25:21 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = 1
+    n, m = len(arr), len(arr[0])
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] != arr[j][i]:
+                answer = 0
+                break
+            break
+    return answer
