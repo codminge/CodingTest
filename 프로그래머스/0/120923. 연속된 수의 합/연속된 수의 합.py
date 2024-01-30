1c15aa3c8e1adb5a5a55744a2366d527f6f65113
Minju Jeong <126869709+codminge@users.noreply.github.com>
2024-01-31 03:25:54 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -2,34 +2,26 @@ def solution(num, total):
     answer = []
     if num % 2 == 1:
         midNum = total // num
-        temp = midNum
-        for i in range(0, num, 1):
-            if i > num//2:
-                temp = temp + 1
-                answer.append(temp)
-                temp = answer[i]
-            if i < num//2:
-                temp = temp - 1
-                answer.append(temp)
-                temp = answer[i]
-            if i == num//2:
-                temp = temp + 1
-                answer.append(midNum)
-                temp = answer[i]
+        answer = calculation(num, midNum)
     else:
         midNum = (total // num) + 1 
-        temp = midNum
-        for j in range(0, num, 1):
-            if j > num//2:
-                temp = temp + 1
-                answer.append(temp)
-                temp = answer[j]
-            if j < num//2:
-                temp = temp - 1
-                answer.append(temp)
-                temp = answer[j]
-            if j == num//2:
-                temp = temp + 1
-                answer.append(midNum)
-                temp = answer[j]
+        answer = calculation(num, midNum)
     return sorted(answer)
+
+def calculation(num, midNum):
+    answer = []
+    temp = midNum
+    for i in range(0, num, 1):
+        if i > num//2:
+            temp = temp + 1
+            answer.append(temp)
+            temp = answer[i]
+        if i < num//2:
+            temp = temp - 1
+            answer.append(temp)
+            temp = answer[i]
+        if i == num//2:
+            temp = temp + 1
+            answer.append(midNum)
+            temp = answer[i]
+    return answer
