feeb9afc51bc099056097f362d023e419287e20e
Minju Jeong <126869709+codminge@users.noreply.github.com>
2024-02-10 21:42:00 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,14 @@
+def solution(my_string):
+    answer = []
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    count = 0
+    
+    for i in alphabet:
+        answer.append(0)
+        for j in my_string:
+            if i == j:
+                index = alphabet.index(j)
+                count = answer[index] + 1
+                answer[index] = count
+                
+    return answer
